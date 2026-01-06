0394e564c3128ebdb76fc90efbf0faa8af5d108e
alisnapro <workspace.auliamaruf@gmail.com>
2026-01-06 12:01:39 +0700

    refactor: Simplify navigation handling and integrate HasPageShield in ManualBook, MonitoringAsetScanner, and TechnicalDocumentation

--- app/Filament/Pages/ManualBook.php
+++ app/Filament/Pages/ManualBook.php
@@ -3,31 +3,22 @@
 namespace App\Filament\Pages;
 
 use Filament\Pages\Page;
-use Filament\Navigation\NavigationItem;
+use BezhanSalleh\FilamentShield\Traits\HasPageShield;
 
 class ManualBook extends Page
 {
+    use HasPageShield;
+
     protected static ?string $navigationIcon = 'heroicon-o-book-open';
     protected static ?string $navigationGroup = 'Dokumen Aplikasi';
     protected static ?string $navigationLabel = 'Manual Book';
     protected static ?string $title = 'Manual Book';
     protected static ?int $navigationSort = 1;
+    protected static ?string $navigationUrl = 'https://docs.google.com/document/d/1BMDEoW551PPhgyGZmNzcLY_TCbe0kNCyhbjQiEfXbM8/edit?usp=sharing';
 
     protected static string $view = 'filament.pages.manual-book';
 
-    public static function getNavigationItems(): array
-    {
-        return [
-            NavigationItem::make(static::getNavigationLabel())
-                ->group(static::getNavigationGroup())
-                ->icon(static::getNavigationIcon())
-                ->sort(static::getNavigationSort())
-                ->url('https://docs.google.com/document/d/1BMDEoW551PPhgyGZmNzcLY_TCbe0kNCyhbjQiEfXbM8/edit?usp=sharing')
-                ->openUrlInNewTab(),
-        ];
-    }
-
-    public static function shouldRegisterNavigation(): bool
+    public static function shouldOpenInNewTab(): bool
     {
         return true;
     }
--- app/Filament/Pages/TechnicalDocumentation.php
+++ app/Filament/Pages/TechnicalDocumentation.php
@@ -3,31 +3,22 @@
 namespace App\Filament\Pages;
 
 use Filament\Pages\Page;
-use Filament\Navigation\NavigationItem;
+use BezhanSalleh\FilamentShield\Traits\HasPageShield;
 
 class TechnicalDocumentation extends Page
 {
+    use HasPageShield;
+
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
     protected static ?string $navigationGroup = 'Dokumen Aplikasi';
     protected static ?string $navigationLabel = 'Technical Documentation';
     protected static ?string $title = 'Technical Documentation';
     protected static ?int $navigationSort = 2;
+    protected static ?string $navigationUrl = 'https://docs.google.com/document/d/19wMNkz7oW29tYP5DRw1B4ab1ty61PpwxMdwotJeCRK0/edit?usp=sharing';
 
     protected static string $view = 'filament.pages.technical-documentation';
 
-    public static function getNavigationItems(): array
-    {
-        return [
-            NavigationItem::make(static::getNavigationLabel())
-                ->group(static::getNavigationGroup())
-                ->icon(static::getNavigationIcon())
-                ->sort(static::getNavigationSort())
-                ->url('https://docs.google.com/document/d/19wMNkz7oW29tYP5DRw1B4ab1ty61PpwxMdwotJeCRK0/edit?usp=sharing')
-                ->openUrlInNewTab(),
-        ];
-    }
-
-    public static function shouldRegisterNavigation(): bool
+    public static function shouldOpenInNewTab(): bool
     {
         return true;
     }
